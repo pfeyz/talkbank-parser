92ed8122ddcbdeae5fab56330fb017c5afed0736
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-02-29 19:02:55 -0500

    more tests, fixtures, using cElement, commit is way too big

--- talkbank_parser.py
+++ talkbank_parser.py
@@ -1,185 +1,270 @@
 # -*- py-which-shell: "python2" -*-
+# -*- coding: utf-8 -*-
+
+"""
+Tools for parsing XML cha files
+
+XML Parsing based on:
+-  http://talkbank.org/software/xsddoc/
+
+schema is here http://talkbank.org/software/talkbank.xsd
+"""
+
 import abc
 import itertools
-import sys
 import unittest
-from collections import namedtuple
-from string import Template
-from xml.etree.ElementTree import ElementTree, dump
+import re
+from xml.etree.cElementTree import ElementTree, dump
 
-import tests
+from datatypes import MorToken
 
-class TagTypeError(Exception):
-    pass
-
-class Parser:
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def parse(self, node):
-        """ Parses the XML-tree rooted at node into objects.
-
-        """
-
-        pass
-
-DEFAULT_NAMESPACE = ""
-"{http://www.talkbank.org/ns/talkbank}"
-
-ns = lambda x: qualify_with_namespace(x, DEFAULT_NAMESPACE)
-
-def set_namespace(namespace):
-    global DEFAULT_NAMESPACE
-    DEFAULT_NAMESPACE = namespace
-
-def qualify_with_namespace(tag, namespace):
-    return "%s%s" % (namespace, tag)
-
-def flatten(listOfLists):
+def flatten(list_of_lists):
     """Flatten one level of nesting
     from python.org
     """
-    return itertools.chain.from_iterable(listOfLists)
+    return itertools.chain.from_iterable(list_of_lists)
 
-def qualify_path(path_string, namespace):
-    return "/".join([qualify_with_namespace(i, namespace)
-                     for i in path_string.split("/")])
+class TagTypeError(Exception):
+    """Raised when parsing function receives an xml element of incorrect
+    tag-type"""
 
-MT = namedtuple("MorToken", "prefix word stem pos subPos sxfx sfx")
+    pass
 
-class MorToken(MT):
-    "Represents an element within an utterance"
+class TalkbankParser(object):
+    """ Parses entire CHA document.
 
-    template = Template("$prefix$pos$subPos|$stem$sxfx$sfx")
+    Maybe it should handle CA as well? Delegates actual parsing to objects that
+    implement the Parser interface.
 
-    def _join_if_any(self, items, joiner):
-        if len(items) == 0:
-            return ""
-        return joiner + joiner.join(items)
-
-    def __repr__(self):
-        s = MorToken.template.substitute(prefix=self._join_if_any(self.prefix, "#"),
-                                         pos=self.pos,
-                                         subPos=self._join_if_any(self.subPos, ":"),
-                                         stem=self.stem,
-                                         sxfx=self._join_if_any(self.sxfx, "&"),
-                                         sfx=self._join_if_any(self.sfx, "-"))
-        return unicode(s)
-
-
-def findall(element, path_string, namespace=DEFAULT_NAMESPACE):
-    """ runs findall on element with a fully namespace qualified version
-    of path_string"""
-
-    return element.findall(qualify_path(path_string, namespace))
-
-def find(element, path_string, namespace=DEFAULT_NAMESPACE):
-    """ runs find on element with a fully namespace qualified version
-    of path_string"""
-
-    return element.find(qualify_path(path_string, namespace))
-
-
-def parse_pos(element):
-    """ Returns the pos and list of subPos found in element.
-
-    element is an xml pos tag Element.
     """
+    def __init__(self, *parsers):
+        self.parsers = parsers
 
-    if element.tag != ns("pos"):
-        raise TagTypeError("Passed non pos-tag to parse_pos")
+    def parse(self, filename):
+        return MorParser().parse(filename)
 
-    try:
-        pos = find(element, "c").text
-    except AttributeError:
-        pos = None
-    subPos = [i.text for i in findall(element, "s")]
+class Parser:
+    """ The Abstract Base Class for a TalkBank (sub-)parser.
 
-    return pos, subPos
-
-def parse_mor_word(text, element):
-    """ Parses a mw element into a MorToken
-
-    args
-      text: the word from the main tier
-      element: the xml element of tag-type mw
+    Convenience methods that are handy for general namespace-qualified XML
+    parsing are defined here as well.
 
     """
 
-    if element.tag != ns("mw"):
-        raise TagTypeError("Passed non mw-tag to parse_mor_word")
+    __metaclass__ = abc.ABCMeta
+    def __init__(self, namespace=""):
+        self.namespace = namespace
+        self.brokens = []
 
-    pos, subPos = parse_pos(find(element, "pos"))
-    try:
-        stem = find(element, "stem").text
-    except AttributeError:
-        stem = None
-    prefix = [i.text for i in findall(element, "mpfx")]
+    @abc.abstractmethod
+    def parse(self, node):
+        "Parses the XML-tree rooted at node into objects."
 
-    suffixes = findall(element, "mk")
-    sxfx = [i.text for i in suffixes if i.get("type") == "sxfx"]
-    sfx = [i.text for i in suffixes if i.get("type") == "sfx"]
+        raise NotImplementedError()
 
-    return MorToken(prefix, text, stem, pos, subPos, sxfx, sfx)
+    def _qualify_path(self, path_string, namespace):
+        return "/".join([self._qualify_with_namespace(i, namespace)
+                         for i in path_string.split("/")])
+    def _qualify_with_namespace(self, tag, namespace):
+        return "%s%s" % (namespace, tag)
 
-def parse_compound(text, compound):
-    if compound.tag != ns("mwc"):
-        raise TagTypeError("Passed non mwc to parse_compound")
+    def _findall(self, element, path_string):
+        """ runs findall on element with a fully namespace qualified version
+        of path_string"""
+        return element.findall(self._qualify_path(path_string, self.namespace))
 
-    prefix = [i.text for i in findall(compound, "mpfx")]
-    pos, subPos = parse_pos(find(compound, "pos"))
-    words = [parse_mor_word("+", i) for i in findall(compound, "mw")]
-    return MorToken(prefix, text, "+".join([w.stem for w in words]),
-                    pos, subPos, "", "")
+    def _find(self, element, path_string):
+        """ runs find on element with a fully namespace qualified version
+        of path_string"""
 
-def parse_clitic(text, element):
-    compound = find(element, "mwc")
-    if compound is not None:
-        return parse_compound(text, compound)
-    word_elem = find(element, "mw")
-    if word_elem is not None:
-        return parse_mor_word(text, word_elem)
+        return element.find(self._qualify_path(path_string, self.namespace))
 
-def parse_mor_element(text, element):
-    """ need to handle mor-pre and mor-post as well as mw """
-    assert(element.tag == ns("mor"))
-    compound = find(element, "mwc")
-    pre_clitics = [parse_clitic("PRE-CLITIC", c) for c in findall(element, "mor-pre")]
-    post_clitics = [parse_clitic("POST-CLITIC", c) for c in findall(element, "mor-post")]
-    if compound is not None:
-        parts = pre_clitics
-        parts.append(parse_compound(text, compound))
-        parts += post_clitics
-    else:
-        parts = pre_clitics
-        parts.append(parse_mor_word(text, find(element, "mw")))
-        parts += post_clitics
-    return parts
+    def ns(self, path):
+        return self._qualify_path(path, self.namespace)
 
-def parse_mor_tier(filename):
-    doc = ElementTree(file=filename)
-    for i in findall(doc, "u"):
-        print i
-        speaker = i.get("who")
-        words = list(flatten(parse_mor_element(j.text, find(j, "mor"))
-                               for j in i if j != None and j.tag == ns("w")))
-        if None in words:
-            print dump(i)
-            print words
-            raise Exception
-        yield speaker, words
-      #   elif j.tag == ns("s"):
-      #     print punct(j.get("type")),
-      #   elif j.tag == ns("t"):
-      #     print endpunct(j.get("type")),
-      # print
+class MorParser(Parser):
+    def parse_pos(self, element):
+        """ Returns the pos and list of subPos found in element.
 
-    # for speaker, utterance in parse_mor_tier(sys.argv[1]):
-    #   print speaker, [unicode(i) for i in utterance]
+        element is an xml pos tag Element.
+
+        """
+
+        if element.tag != self.ns("pos"):
+            raise TagTypeError("Passed non pos-tag to parse_pos")
+
+        try:
+            pos = self._find(element, "c").text
+        except AttributeError:
+            pos = None
+        subPos = [i.text for i in self._findall(element, "s")]
+
+        return pos, subPos
+
+    def parse_mor_word(self, text, element):
+        """ Parses a mw element into a MorToken
+
+        args
+          text: the word from the main tier
+          element: the xml element of tag-type mw
+
+        """
+
+        if element.tag != self.ns("mw"):
+            raise TagTypeError("Passed non mw-tag to parse_mor_word")
+
+        pos, subPos = self.parse_pos(self._find(element, "pos"))
+        try:
+            stem = self._find(element, "stem").text
+            stem = self.remove_bad_symbols(stem)
+        except AttributeError:
+            stem = None
+        prefix = [i.text for i in self._findall(element, "mpfx")]
+
+        suffixes = self._findall(element, "mk")
+        sxfx = [i.text for i in suffixes if i.get("type") == "sxfx"]
+        sfx = [i.text for i in suffixes if i.get("type") == "sfx"]
+
+        return MorToken(prefix, text, stem, pos, subPos, sxfx, sfx)
+
+    def parse_compound(self, text, compound):
+        if compound.tag != self.ns("mwc"):
+            raise TagTypeError("Passed non mwc to parse_compound")
+
+        prefix = [i.text for i in self._findall(compound, "mpfx")]
+        pos, subPos = self.parse_pos(self._find(compound, "pos"))
+        words = [self.parse_mor_word("+", i)
+                 for i in self._findall(compound, "mw")]
+        return MorToken(prefix, text, "+".join([w.stem for w in words]),
+                        pos, subPos, "", "")
+
+    def parse_clitic(self, text, element):
+        compound = self._find(element, "mwc")
+        if compound is not None:
+            return self.parse_compound(text, compound)
+        word_elem = self._find(element, "mw")
+        if word_elem is not None:
+            return self.parse_mor_word(text, word_elem)
+
+    def split_clitic_wordform(self, text):
+        """ expands contracted and possesive words
+
+        args:
+          text: a word
+
+        returns:
+          A two-tuple of (base, post-clitic). This is english-centric.
+
+        tokenization algorithm taken from:
+            http://www.cis.upenn.edu/~treebank/tokenization.html
+        """
+
+        if text is None:
+            return None, None
+        # not sure if the s' makes sense.
+        tails = ["('ll)", "('re)", "('ve)", "(n't)", "('LL)",
+                 "('RE)", "('VE)", "(N'T)", r"('[sSmMdD])", "(s')$"]
+
+        unmarked = [["([Cc])annot", r"\1na not"],
+                    ["([Dd])'ye", r"\1' ye"],
+                    ["([Gg])imme", r"\1im me"],
+                    ["([Gg])onna", r"\1on na"],
+                    ["([Gg])otta", r"\1ot ta"],
+                    ["([Ll])emme", r"\1em me"],
+                    ["([Mm])ore'n", r"\1or 'n"],
+                    ["'([Tt])is", r"'\1 is"],
+                    ["'([Tt])was", r"'\1 was"],
+                    ["([Ww])anna", r"\1an na"]]
+
+        encliticsFound = 0
+        for pattern in tails + [pat for pat, word in unmarked]:
+            if re.search(pattern, text):
+                encliticsFound += 1
+        del word
+
+        if encliticsFound > 1:
+            # HACK (maybe)
+            # MOR seems to always tag multi-enclitics as unk anyway.
+            return text, []
+
+        if encliticsFound:
+            for tail in tails:
+                if re.search(tail, text):
+                    parts = re.split(tail, text)[:-1]
+            for pattern, rewrite in unmarked:
+                if re.search(pattern, text):
+                    parts = re.sub(pattern, rewrite, text).split(' ')
+            if len(parts) > 1:
+                return parts[0], parts[1:]
+        return text, []
+
+    def parse_mor_element(self, text, element):
+        """ need to handle mor-pre and mor-post as well as mw """
+        assert(element.tag == self.ns("mor"))
+        compound = self._find(element, "mwc")
+        base_word, post_clitic_words = self.split_clitic_wordform(text)
+
+        pre_clitics = [self.parse_clitic("PRE-CLITIC", c)
+                       for c in self._findall(element, "mor-pre")]
+        try:
+            post_clitics = [self.parse_clitic(post_clitic_words.pop(), c)
+                            for c in self._findall(element, "mor-post")]
+        except IndexError:
+            print text
+            dump(element)
+
+            post_clitics = [self.parse_clitic("?", c)
+                            for c in self._findall(element, "mor-post")]
+
+        assert(len(post_clitics) < 2)
+        if compound is not None:
+            parts = pre_clitics
+            parts.append(self.parse_compound(base_word, compound))
+            parts += post_clitics
+        else:
+            parts = pre_clitics
+            parts.append(self.parse_mor_word(base_word,
+                                             self._find(element, "mw")))
+            parts += post_clitics
+        return parts
+
+    def remove_bad_symbols(self, text):
+        return re.sub(u"\u0294", "", text)
+        # if text[0].encode("utf-8") == u"\u0294".encode('utf8'):  # ʔ
+        #     text = text[1:]
+        # return text
+
+    def extract_word(self, mw_element):
+        text = mw_element.text + "".join([p.tail
+                                    for p in list(mw_element)
+                                    if p.tail is not None])
+        text = self.remove_bad_symbols(text)
+        return text
+
+    def parse(self, filename):
+        doc = ElementTree(file=filename)
+        for utterance in self._findall(doc, "u"):
+            speaker = utterance.get("who")
+
+            words = list(flatten(
+                self.parse_mor_element(self.extract_word(word),
+                                       self._find(word, "mor"))
+                for word in utterance if word is not None \
+                and word.tag == self.ns("w") and len(word) > 0))
+            yield speaker, words
+          #   elif j.tag == ns("s"):
+          #     print punct(j.get("type")),
+          #   elif j.tag == ns("t"):
+          #     print endpunct(j.get("type")),
+          # print
+
+        # for speaker, utterance in parse_mor_tier(sys.argv[1]):
+        #   print speaker, [unicode(i) for i in utterance]
 
 
     # # print "*%s:\t" % speaker , " ".join(unicode(word.word) for word in utterance if word is not None)
     # # print "%mor:\t", " ".join(unicode(word) for word in utterance)
 
 if __name__ == "__main__":
+    import tests
     unittest.main(tests)
--- tests.py
+++ tests.py
@@ -1,11 +1,10 @@
-import pdb
 import unittest
-import talkbank_parser
 from os import path
-from talkbank_parser import findall, parse_mor_element
-from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import ElementTree, dump
+import pdb
 
-talkbank_parser.set_namespace("")
+from talkbank_parser import MorParser
+
 
 class TalkbankParserTest(unittest.TestCase):
     def setUp(self):
@@ -13,15 +12,41 @@ class TalkbankParserTest(unittest.TestCase):
                                                     "compounds.xml"))
         self.clitics = ElementTree(file=path.join("fixtures",
                                                   "clitics.xml"))
-
+        self.utterances = ElementTree(file=path.join("fixtures",
+                                                     "utterances.xml"))
     def test_compound(self):
-        #pdb.set_trace()
         for word in self.compounds.findall("w/mor"):
-            tokens = parse_mor_element(None, word)
+            parser = MorParser(namespace="")
+            tokens = parser.parse_mor_element(None, word)
             for token in tokens:
                 self.assertGreaterEqual(token.stem.count("+"), 1)
 
     def test_clitics(self):
-        for word in self.clitics.findall("w/mor"):
-            tokens = parse_mor_element(None, word)
-            self.assertGreater(len(tokens), 1)
+        # for word in self.clitics.findall("w/mor"):
+        #     parser = MorParser(namespace="")
+        #     tokens = parser.parse_mor_element(None, word)
+        #     self.assertGreater(len(tokens), 1)
+        #     print tokens
+        parser = MorParser("{http://www.talkbank.org/ns/talkbank}")
+        for i in parser.parse("fixtures/clitics.xml"):
+            print i
+        head, tail = parser.split_clitic_wordform("that's")
+        self.assertEqual(head, "that")
+        self.assertEqual(tail, ["'s"])
+
+    def test_sentence(self):
+        for word in self.compounds.findall("w/mor"):
+            parser = MorParser(namespace="")
+            tokens = parser.parse_mor_element(None, word)
+            self.assertGreaterEqual(len(tokens), 1)
+
+    def test_document(self):
+        parser = MorParser("{http://www.talkbank.org/ns/talkbank}")
+        for i in parser.parse("fixtures/test_doc.xml"):
+            i
+
+
+    # @unittest.skip
+    # def test_full_doc(self):
+    #     parser = MorParser()
+    #     print list(parser.parse("/home/paul/corpora/talkbank-xml/SBCSAE/01.xml"))
