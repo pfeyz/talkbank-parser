35aac57e2eebcf6de631f93ea05b70e99176e624
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-04-25 02:44:10 -0400

    fix bug in handling replacements

--- talkbank_parser.py
+++ talkbank_parser.py
@@ -268,8 +268,14 @@ class MorParser(Parser):
                 if word is None or len(word) == 0:
                     continue
                 if word.tag == self.ns("w"):
-                    words.append(self.parse_mor_element(self.extract_word(word),
-                                                        self._find(word, "mor")))
+                    replacement = self._find(word, "replacement")
+                    if replacement:
+                        for rep_word in self._findall(replacement, "w"):
+                            words.append(self.parse_mor_element(self.extract_word(rep_word),
+                                                                self._find(rep_word, "mor")))
+                    else:
+                        words.append(self.parse_mor_element(self.extract_word(word),
+                                                            self._find(word, "mor")))
                 if word.tag == self.ns("t"):
                     punct = punctuation.get(word.get("type"), "-")
                     words.append([MorToken("", punct, punct, punct, "", "", "")])
