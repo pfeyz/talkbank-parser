5c7b6d543b8a7fd6534caea7540ecb8b93d6a948
Jason Xie <jyx6@case.edu>
2016-05-19 13:24:57 -0400

    implement setup for talkbank_parser

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='talkbank_parser',
+      description='The funniest joke in the world',
+      url='https://bitbucket.org/pfeyz/talkbank-parser.git',
+      author='Paul Feitzinger',
+      packages=['talkbank_parser'])
