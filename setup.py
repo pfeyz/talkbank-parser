c0a7e879c9db0d5022f56b956871dc140db0d747
Paul Feitzinger <paul@pfeyz.com>
2018-01-19 16:07:56 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(version='0.0.4',
+setup(version='0.0.5',
       name='talkbank_parser',
       description='Tools for parsing Talkbank XML corpora.',
       url='https://bitbucket.org/pfeyz/talkbank-parser.git',
